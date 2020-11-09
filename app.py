fec650db42e7ea0a0410b685fe01790e29405b6c
ViktoriaOhorilko <viktoriaohorilko@gmail.com>
2020-11-10 01:31:27 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/Vika', methods=['GET'])
 def hello_world():
-    return 'Hello Rabotyahy!'
+    lon=request.args.get('lon')
+    lat=request.args.get('lat')
+    return 'Your placement :  • longitude = ' + lon + '    •latitude = ' + lat
 
 
 if __name__ == '__main__':
