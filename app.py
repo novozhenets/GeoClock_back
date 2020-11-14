5fbf5d022ce9c0ecb8eafdfb71690d7d7a9cf36b
BodiaPavlyk <bogdan2002mih@gmail.com>
2020-11-14 02:40:03 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask,jsonify,request,render_template
+from flask import Flask
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 
-@app.route('/Bodia', methods=['GET'])
-def hello_world():
-    test = request.args.get('test', '')
-    return "test = " + test
+@manager.command
+def db_create():
+    db.create_all()
+
+
+@app.route("/Bodia")
+def Hello():
+    return "Hello world!"
 
 
 if __name__ == '__main__':
