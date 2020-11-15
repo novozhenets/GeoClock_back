125de589fcabd739d2806918943dcb5193896a47
BodiaPavlyk <bogdan2002mih@gmail.com>
2020-11-16 00:58:49 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, json, request
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
@@ -13,14 +13,112 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+
 @manager.command
 def db_create():
     db.create_all()
 
 
-@app.route("/Sonia")
+@app.route("/start")
 def Hello():
-    return "Hello to everyone!"
+    return "Hello to everyoneeee!"
+
+
+
+#link to try: http://127.0.0.1:5000/UserC?login=Buka&password=1111
+@app.route('/UserC', methods=['GET'])
+def hello_user():
+    user_data = request.args
+    user_controler = UserController()
+    if user_controler.create(user_data):
+        return "Success!"
+    else:
+        return "Create failed!"
+
+
+#link to try: http://127.0.0.1:5000/GeoC?lat=14.19.25&lon=47.21.52&radius=24
+@app.route('/GeoC', methods=['GET'])
+def hello_geo():
+    geo_data = request.args
+    geo_controler = GeolocationController()
+    if geo_controler.create(geo_data):
+        return "Success!"
+    else:
+        return "Create failed!"
+
+
+#link to try: http://127.0.0.1:5000/NotC?not=Have_a_good_day)
+@app.route('/NotC', methods=['GET'])
+def hello_not():
+    not_data = request.args
+    not_controler = NotificationController()
+    if not_controler.create(not_data):
+        return "Success!"
+    else:
+        return "Create failed!"
+
+
+class ModelUser(object):
+
+    def __init__(self, user_login=None, user_password=None):
+        self.user_login = user_login
+        self.user_password = user_password
+
+
+class ModelGeolocation(object):
+
+    def __init__(self, latitude=None, longitude=None, radius=None):
+        self.latitude = latitude
+        self.longitude = longitude
+        self.radius = radius
+
+
+class ModelNotification(object):
+
+    def __init__(self, notification=None):
+        self.notification = notification
+
+
+class UserController(object):
+
+    def __init__(self, model_user=ModelUser()):
+        self.model_user = model_user
+
+    def create(self, user_data=None):
+        self.model_user.user_login = user_data.get('login')
+        self.model_user.user_password = user_data.get('password')
+        if self.model_user.user_login != None and self.model_user.user_password != None:
+            return 1
+        else:
+            return 0
+
+
+class GeolocationController(object):
+
+    def __init__(self, model_geolocation=ModelGeolocation()):
+        self.model_geolocation = model_geolocation
+
+    def create(self,geo_data=None):
+        self.model_geolocation.latitude=geo_data.get('lat')
+        self.model_geolocation.longitude=geo_data.get('lon')
+        self.model_geolocation.radius=geo_data.get('radius')
+        if self.model_geolocation.latitude != None and self.model_geolocation.longitude!=None and self.model_geolocation.radius!=None:
+            return 1
+        else:
+            return 0
+
+
+class NotificationController(object):
+
+    def __init__(self, model_notification=ModelNotification()):
+        self.model_notification = model_notification
+
+    def create(self,not_data=None):
+        self.model_notification.notification=not_data.get('not')
+        if self.model_notification.notification!=None:
+            return 1
+        else:
+            return 0
 
 
 if __name__ == '__main__':
---
+++ migrations/versions/68755cd663d6_initial_migration.py
@@ -0,0 +1,24 @@
+"""Initial migration
+
+Revision ID: 68755cd663d6
+Revises: 
+Create Date: 2020-11-16 00:57:22.136575
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '68755cd663d6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
