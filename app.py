333c1a04778084590ef8c842d83b90a191fd60c3
ViktoriaOhorilko <viktoriaohorilko@gmail.com>
2020-11-15 18:32:53 +0200

    initial commit

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from flask import Flask, request
+from flask import Flask
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+#tyt buv ja
+#dubl 2.0
+
+@manager.command
+def db_create():
+    db.create_all()
 
 
-@app.route('/Vika', methods=['GET'])
-def hello_world():
-    lon=request.args.get('lon')
-    lat=request.args.get('lat')
-    return 'Your placement :  • longitude = ' + lon + '    •latitude = ' + lat
+@app.route("/Sonia")
+def Hello():
+    return "Hello robotiady!"
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
